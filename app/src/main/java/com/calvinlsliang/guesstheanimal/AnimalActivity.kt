ca4e31bfc8e24031081367d3922880160eb3c90c
Calvin Liang <calvin@nextdoor.com>
2018-11-23 23:25:04 -0800

    Square image view is not working for dynamic widths

--- app/src/main/java/com/calvinlsliang/guesstheanimal/AnimalActivity.kt
+++ app/src/main/java/com/calvinlsliang/guesstheanimal/AnimalActivity.kt
@@ -2,18 +2,32 @@ package com.calvinlsliang.guesstheanimal
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.squareup.picasso.Picasso
+import com.calvinlsliang.guesstheanimal.adapter.AnimalsAdapter
+import com.calvinlsliang.guesstheanimal.adapter.AnimalsLayoutManager
 import kotlinx.android.synthetic.main.activity_animal.*
 
 class AnimalActivity : AppCompatActivity() {
+
+    lateinit var adapter: AnimalsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_animal)
 
-        Picasso.get()
-                .load("https://i.imgur.com/O4yTEKg.jpg")
-                .into(animal_picture)
+        setupAdapter()
+
+    }
+
+    private fun setupAdapter() {
+        adapter = AnimalsAdapter()
+        adapter.animals = listOf("https://i.imgur.com/xQCI355.jpg",
+                "https://i.imgur.com/i4zsvJ1.jpg?1",
+                "https://i.imgur.com/Zoif7Do.jpg",
+                "https://i.imgur.com/EEgAhlI.jpg",
+                "https://i.imgur.com/aHgda3v.jpg",
+                "https://i.imgur.com/xGKcKPX.jpg")
+        animals_recycler_view.adapter = adapter
+        animals_recycler_view.layoutManager = AnimalsLayoutManager(this, 2)
 
     }
 }
---
+++ app/src/main/java/com/calvinlsliang/guesstheanimal/adapter/AnimalsAdapter.kt
@@ -0,0 +1,38 @@
+package com.calvinlsliang.guesstheanimal.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.calvinlsliang.guesstheanimal.R
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.animal_cell.view.*
+
+class AnimalsAdapter : RecyclerView.Adapter<AnimalsViewHolder>() {
+
+    var animals = listOf<String>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimalsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.animal_cell, parent, false)
+        return AnimalsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AnimalsViewHolder, position: Int) {
+        holder.bind(animals.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return animals.count()
+    }
+}
+
+class AnimalsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(animal: String) {
+        Picasso.get()
+                .load(animal)
+                .error(R.mipmap.ic_launcher)
+                .fit()
+                .into(itemView.animal_image)
+    }
+
+}
